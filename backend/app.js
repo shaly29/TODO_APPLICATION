d7dc3d46e22b939bb262d714cd19432cabf80089
NISHALINI <nishalinithirukkumar2@gmail.com>
2024-06-10 17:23:15 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 const express = require("express")
-
+const bcrypt = require("bcrypt");
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose'); 
 require('dotenv').config(); 
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoute');
-// const userRoutes= require('./routes/userRoute')
+const userRoutes= require('./routes/userRoute')
 
 const app = express(); 
 app.use(bodyParser.json());
@@ -17,7 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = 4007; 
 app.use('/api', todoRoutes);
-// app.use('api/v1/',userRoutes)
+app.use('api/v1/',userRoutes)
+
 const connectionString = process.env.MONGODB_URL; 
 mongoose.connect(connectionString)
 .then(() => console.log('Connected to the database…'))
